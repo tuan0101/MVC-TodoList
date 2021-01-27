681e802e3854a548c4786e4cb2e215c714c9fb3e
Tuan Pham <tuan.programmer0101@gmail.com>
2021-01-27 08:28:34 -0500

    Set Up Layout
    Removed Privacy page
    Added Todo-Item page

---
+++ MVC-TodoList/Controllers/ItemsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_TodoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
