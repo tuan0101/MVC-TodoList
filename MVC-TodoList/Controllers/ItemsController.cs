b79da1e76e093fb3490c0b7da31ed2aefad31c5c
Tuan Pham <tuan.programmer0101@gmail.com>
2021-01-27 09:07:52 -0500

    Added API calls to ItemsController
    [HttpGet]
    [HttpDelete]

--- MVC-TodoList/Controllers/ItemsController.cs
+++ MVC-TodoList/Controllers/ItemsController.cs
@@ -1,16 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_TodoList.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MVC_TodoList.Controllers
 {
     public class ItemsController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ItemsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Items.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Items.FirstOrDefaultAsync(item => item.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Items.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+        #endregion
     }
 }
