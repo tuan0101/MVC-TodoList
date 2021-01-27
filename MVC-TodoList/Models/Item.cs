59a50ea00494609919f38e01a99ea243e976d7ba
Tuan Pham <tuan.programmer0101@gmail.com>
2021-01-26 23:50:49 -0500

    Create Item Model

---
+++ MVC-TodoList/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_TodoList.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime DueDate { get; set; }
+        public string Priority { get; set; }
+        public bool Status { get; set; }
+    }
+}
